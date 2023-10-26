9b907a76eb429666cb561b2164e544d58712fed6
tognoek <zgye28102003@gmail.com>
2023-10-26 07:10:37 +0700

    26Thang10

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -75,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, DangSanPhamActivity.class));
-                startActivity(new Intent(MainActivity.this, detail_shop.class));
             }
         });
     }
