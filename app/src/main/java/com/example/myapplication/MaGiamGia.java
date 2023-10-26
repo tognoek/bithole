b23e0daa9eb50dcf609c3ec5ae0c71b87891cb88
thongmloe <hoanggvannthongg@gmail.com>
2023-10-27 00:03:49 +0700

    trang ma giam gia Le Huu Thi

---
+++ app/src/main/java/com/example/myapplication/MaGiamGia.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaGiamGia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ma_giam_gia);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.nextpage).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, GioHang.class));
+                startActivity(new Intent(MainActivity.this, MaGiamGia.class));
             }
         });
     }
