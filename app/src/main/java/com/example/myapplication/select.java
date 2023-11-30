6bb700defbb0c2e4f755258c6e32edeed1a010e9
thongmloe <hoanggvannthongg@gmail.com>
2023-11-30 21:20:33 +0700

    test

--- app/src/main/java/com/example/myapplication/select.java
+++ app/src/main/java/com/example/myapplication/select.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -30,9 +32,11 @@ public class select extends AppCompatActivity {
     private ImageView imageView_caidat;
     private ImageView imageView_trove;
     private LinearLayout linear_chitietsp;
+    private EditText inputText;
     private ExpandableHeightGridView gridView;
     private AdapterSanPham adapterSanPham;
     private ArrayList<SanPham> listSanPham;
+    private ArrayList<SanPham> listTimKiem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +60,10 @@ public class select extends AppCompatActivity {
 
     private void doDuLieuVaoAdapter() {
         listSanPham = new ArrayList<>();
-        adapterSanPham = new AdapterSanPham(this, R.layout.layout_item, listSanPham);
+//        adapterSanPham = new AdapterSanPham(this, R.layout.layout_item, listSanPham);
+//        gridView.setAdapter(adapterSanPham);
+        listTimKiem = new ArrayList<>(listSanPham);
+        adapterSanPham = new AdapterSanPham(this, R.layout.layout_item, listTimKiem);
         gridView.setAdapter(adapterSanPham);
     }
 
@@ -71,7 +78,7 @@ public class select extends AppCompatActivity {
                     SanPham sanPham = postSnapshot.getValue(SanPham.class);
                     listSanPham.add(sanPham);
                 }
-                adapterSanPham.notifyDataSetChanged();
+//                adapterSanPham.notifyDataSetChanged();
             }
 
             @Override
@@ -90,6 +97,23 @@ public class select extends AppCompatActivity {
         imageView_trove.setOnClickListener(view ->
                 getOnBackPressedDispatcher().onBackPressed()
         );
+        inputText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
     private void anhXa(){
@@ -98,5 +122,29 @@ public class select extends AppCompatActivity {
         imageView_caidat = findViewById(R.id.img_caidat);
         imageView_trove = findViewById(R.id.img_trove);
         linear_chitietsp = findViewById(R.id.chitiet_sp);
+        inputText = findViewById(R.id.editTextInput);
+    }
+    private void filter(CharSequence text){
+        listTimKiem.clear();
+        String query = text.toString().toLowerCase().trim();
+        if(query.isEmpty()){
+            listTimKiem.addAll(listSanPham);
+        }else {
+            for(SanPham sanPham : listSanPham){
+                boolean isMatch = true;
+                String[] keys = query.split("\\s+");
+                for(String key : keys){
+                    if(!sanPham.getName().toLowerCase().contains(key)){
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch){
+                    listTimKiem.add(sanPham);
+                }
+
+            }
+        }
+        adapterSanPham.notifyDataSetChanged();
     }
 }
